af017ab01002e10b057f4fb3a987eb875d1f671f
Karlatemp <kar@kasukusakura.com>
2022-10-29 15:25:17 +0800

    upload console plugin jar

--- .scripts/ci-dummy-sign.sh
+++ .scripts/ci-dummy-sign.sh
@@ -3,6 +3,8 @@
 mkdir build
 mkdir build/output
 
+cp mirai-login-solver-sakura/mirai/* build/output
+
 apksigner sign -v --key keys/ci-dummy.key.pkcs8 --cert keys/ci-dummy.crt --out build/output/apk-debug-cidummy.apk android/app/build/outputs/apk/debug/app-debug.apk
 
 if [ -f "keys/org.key.pkcs8" ]; then
